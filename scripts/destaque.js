b8164db872741936e3e5cc7ee461146b09b8343e
Robson <robsnmchado@gmail.com>
2025-05-01 16:39:52 -0300

    Implementação da seção de imóveis em destaque

---
+++ scripts/destaque.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    navigation: {
+        nextEl: ".setad",
+        prevEl: ".setae",
+    },
+});

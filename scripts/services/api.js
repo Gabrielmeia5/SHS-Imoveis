7f393dd2f347028853fb0093195c4e0159e09f5a
Gabriel Lima <ogabriellima1991@gmail.com>
2025-05-11 18:51:01 -0300

    Teste .json pasta index

--- scripts/services/api.js
+++ scripts/services/api.js
@@ -1,6 +1,6 @@
 export async function fetchImoveis() {
   try {
-    const response = await fetch('../data/db.json');
+    const response = await fetch('./db.json');
     const data = await response.json();
     return data;
   } catch (error) {
